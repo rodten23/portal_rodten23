36fc0f54e71b933fb27af50a6e62cbb21afeed01
rodten23 <rodten23@gmail.com>
2023-12-29 14:02:29 -0300

    :sparkles: feat: Importação dos módulos e configuração inicial para envio de e-mail pelo formulário.

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, redirect
+from flask_mail import Mail, Message
+from config import senha_transacoes, meu_email, senha_meu_email
 
 app = Flask(__name__)
+app.secret_key = senha_transacoes
+
+mail_settings = {
+    "MAIL_SERVER": "smtp.gmail.com",
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": meu_email,
+    "MAIL_PASSWORD": senha_meu_email
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
