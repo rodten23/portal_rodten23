2be1ff4b828b0c277a816e656e1ff5526b5caa69
Rodrigo Alves Tenório <rodten23@yahoo.com.br>
2026-08-05 19:26:04 -0300

    :recycle: refactor: Ajuste de formatação.

--- portal_rodten23/contract_clicksign/contract_datetime.py
+++ portal_rodten23/contract_clicksign/contract_datetime.py
@@ -1,12 +1,10 @@
 import datetime as dt
 
+
 def create_deadline():
     current_date = dt.date.today()
     zero_hour = dt.time(0, 0, 0, 0)
     future_date = current_date + dt.timedelta(days=30)
     deadline = f'{future_date}T{zero_hour}.000-03:00'
 
-    return {
-        'current_date': current_date,
-        'deadline': deadline
-    }
+    return {'current_date': current_date, 'deadline': deadline}
