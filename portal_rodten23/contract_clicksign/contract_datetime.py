292a070e700460bc230d488737165af83ede7a8c
Rodrigo Alves Tenório <rodten23@yahoo.com.br>
2026-08-05 13:43:16 -0300

    :recycle: refactor: Isolamento da função para data limite de assinatura.

---
+++ portal_rodten23/contract_clicksign/contract_datetime.py
@@ -0,0 +1,9 @@
+import datetime as dt
+
+def create_deadline():
+    current_date = dt.date.today()
+    zero_hour = dt.time(0, 0, 0, 0)
+    future_date = current_date + dt.timedelta(days=30)
+    deadline = f'{future_date}T{zero_hour}.000-03:00'
+
+    return deadline
