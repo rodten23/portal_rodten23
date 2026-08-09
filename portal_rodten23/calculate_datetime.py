c4ff0b4777feb9c258d9e43bc17333af718874e5
Rodrigo Alves Tenório <rodten23@gmail.com>
2026-08-09 18:11:44 -0300

    :recycle: refactor: Ajuste no import para datetime.

--- portal_rodten23/calculate_datetime.py
+++ portal_rodten23/calculate_datetime.py
@@ -1,8 +1,8 @@
-import datetime as dt
+from datetime import date, time, timedelta
 
 
 def calculate_age(current_date, year_birth, month_birth, day_birth):
-    count_in_days = current_date - dt(
+    count_in_days = current_date - date(
         int(year_birth), int(month_birth), int(day_birth)
     )
     division_into_years = int(count_in_days.days / 365)
@@ -14,9 +14,9 @@ def calculate_age(current_date, year_birth, month_birth, day_birth):
 
 
 def create_deadline():
-    current_date = dt.date.today()
-    zero_hour = dt.time(0, 0, 0, 0)
-    future_date = current_date + dt.timedelta(days=30)
+    current_date = date.today()
+    zero_hour = time(0, 0, 0, 0)
+    future_date = current_date + timedelta(days=30)
     deadline = f'{future_date}T{zero_hour}.000-03:00'
 
     return {'current_date': current_date, 'deadline': deadline}
