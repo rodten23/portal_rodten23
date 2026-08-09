acf3ed53c1708c2f3e7500d5aeccb8efb71374ab
Rodrigo Alves Tenório <rodten23@gmail.com>
2026-08-09 11:47:37 -0300

    :sparkles: feat: Volta do código para Tente novamente e limpeza do formulário em caso de erro, seguindo timeout de 5 segundos.

--- portal_rodten23/static/script_contract.js
+++ portal_rodten23/static/script_contract.js
@@ -49,18 +49,20 @@ form.addEventListener('submit', (e) => {
         if (data.redirect) {
             window.location.href = data.redirect;
         } else {
-            console.error('O Flask não enviou a chave redirect:', data)
-            btn.textContent = 'Tente novamente!';
-            btn.disabled = false;
-            // form.reset();
-            // email.classList.remove('is-valid');
-            // email.classList.remove('is-invalid');
-            // terms.classList.remove('is-valid');
-            // terms.classList.remove('is-invalid'); 
+            setTimeout(() => {
+                console.error('O Flask não enviou a chave redirect:', data)
+                btn.textContent = 'Tente novamente!';
+                btn.disabled = false;
+                form.reset();
+                email.classList.remove('is-valid');
+                email.classList.remove('is-invalid');
+                terms.classList.remove('is-valid');
+                terms.classList.remove('is-invalid');
+            }, 5000);
         }
     })
     .catch(error => {
-        console.error('Erro:', Error);
+        console.error('Erro:', error);
         btn.textContent = 'Erro ao criar contrato!';
         btn.disabled = false;
     });
